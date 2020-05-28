7a20158db91c70a781f5acccb26a8d4530441430
irina_ubuntu <irina@me.com>
2020-05-28 15:56:38 +0300

    add web dependency and controller that uses clients

---
+++ test2-client/src/main/java/com/mytests/spring/feign/test2client/ClientsController.java
@@ -0,0 +1,20 @@
+package com.mytests.spring.feign.test2client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientsController {
+
+    @Autowired
+    private FeignClient2 test22;
+    @Autowired
+    private FeignClient1 test2;
+
+    @GetMapping("/client2/hello/{name}")
+    public String hello(@PathVariable String name){
+      return test22.hello(name);
+    }
+}
