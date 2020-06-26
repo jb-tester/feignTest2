2c049a3573ba96d7807de73bb328274d199f7952
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2020-06-26 19:44:11 +0300

    constants in paths

--- test2-client/src/main/java/com/mytests/spring/feign/test2client/FeignClient2.java
+++ test2-client/src/main/java/com/mytests/spring/feign/test2client/FeignClient2.java
@@ -4,14 +4,20 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "test22", url="http://localhost:8081/test2/api")
+@FeignClient(name = "test22", url = "http://localhost:8081/test2/api")
 @RequestMapping("/c2")
 public interface FeignClient2 {
+
+    String PREFIX2 = "/bye";
+    String POSTFIX2 = "/{name}";
+    String PATH = PREFIX2 + POSTFIX2;
+    String C_2 = "/c2";
 
     @GetMapping("/hello/{name}")
     String hello(@PathVariable String name);
 
-    @GetMapping("/bye/{name}")
+    @RequestMapping(path = PATH, method = RequestMethod.GET)
     String bye(@PathVariable String name);
 }
